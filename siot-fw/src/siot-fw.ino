30ed7f229627871d7b47d13fe3bf5d91571c99bc
Cliff Brake <cbrake@bec-systems.com>
2019-11-14 14:58:23 -0500

    start of BLE functionality

--- siot-fw/src/siot-fw.ino
+++ siot-fw/src/siot-fw.ino
@@ -1,4 +1,5 @@
 #define ARDUINOJSON_ENABLE_PROGMEM 0
+#include "DiagnosticsHelperRK.h"
 #include "JsonParserGeneratorRK.h"
 #include "PublishQueueAsyncRK.h"
 
@@ -31,6 +32,27 @@ OneWireManager oneWireManager = OneWireManager();
 uint8_t publishQueueRetainedBuffer[2048];
 PublishQueueAsync publishQueue(publishQueueRetainedBuffer, sizeof(publishQueueRetainedBuffer));
 
+const BleUuid serviceUuid("5c1b9a0d-b5be-4a40-8f7a-66b36d0a5176");
+
+BleCharacteristic uptimeCharacteristic("uptime", BleCharacteristicProperty::NOTIFY, BleUuid("fdcf4a3f-3fed-4ed2-84e6-04bbb9ae04d4"), serviceUuid);
+BleCharacteristic signalStrengthCharacteristic("strength", BleCharacteristicProperty::NOTIFY, BleUuid("cc97c20c-5822-4800-ade5-1f661d2133ee"), serviceUuid);
+BleCharacteristic freeMemoryCharacteristic("freeMemory", BleCharacteristicProperty::NOTIFY, BleUuid("d2b26bf3-9792-42fc-9e8a-41f6107df04c"), serviceUuid);
+
+void configureBLE()
+{
+    BLE.addCharacteristic(uptimeCharacteristic);
+    BLE.addCharacteristic(signalStrengthCharacteristic);
+    BLE.addCharacteristic(freeMemoryCharacteristic);
+
+    BleAdvertisingData advData;
+
+    // Advertise our private service only
+    advData.appendServiceUUID(serviceUuid);
+
+    // Continuously advertise when not connected
+    BLE.advertise(&advData);
+}
+
 void setup()
 {
     Serial.begin(115200);
@@ -59,9 +81,11 @@ void setup()
     }
 
     Particle.connect();
+
+    configureBLE();
 }
 
-const unsigned long UPDATE_INTERVAL = 3000;
+const unsigned long UPDATE_INTERVAL = 5000;
 const unsigned long PUBLISH_INTERVAL = 10 * 1000;
 unsigned long lastUpdate = 0 - UPDATE_INTERVAL;
 unsigned long lastPublish = 0 - PUBLISH_INTERVAL;
@@ -114,6 +138,19 @@ void loop()
                 Serial.println("Warning, read loop is not terminating properly");
                 break;
             }
+        }
+
+        if (BLE.connected()) {
+            uint8_t uptime = (uint8_t)DiagnosticsHelper::getValue(DIAG_ID_SYSTEM_UPTIME);
+            uptimeCharacteristic.setValue(uptime);
+
+            uint8_t signalStrength = (uint8_t)(DiagnosticsHelper::getValue(DIAG_ID_NETWORK_SIGNAL_STRENGTH) >> 8);
+            signalStrengthCharacteristic.setValue(signalStrength);
+
+            int32_t usedRAM = DiagnosticsHelper::getValue(DIAG_ID_SYSTEM_USED_RAM);
+            int32_t totalRAM = DiagnosticsHelper::getValue(DIAG_ID_SYSTEM_TOTAL_RAM);
+            int32_t freeMem = (totalRAM - usedRAM);
+            freeMemoryCharacteristic.setValue(freeMem);
         }
 
         /*
