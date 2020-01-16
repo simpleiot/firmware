63f2ce79a1117d415f87b3f1686b0409eac8a360
Cliff Brake <cbrake@bec-systems.com>
2020-01-16 11:33:22 -0500

    updates to test app

--- particle-test/src/particle-test.ino
+++ particle-test/src/particle-test.ino
@@ -5,13 +5,22 @@
  * Date:
  */
 
+#include "dct.h"
+
+SYSTEM_THREAD(ENABLED);
+SYSTEM_MODE(MANUAL);
+
 // setup() runs once, when the device is first turned on.
-void setup() {
+void setup()
+{
+  const uint8_t val = 0x01;
+  dct_write_app_data(&val, DCT_SETUP_DONE_OFFSET, 1);
   Serial.begin(115200);
 }
 
 // loop() runs over and over again, as quickly as it can execute.
-void loop() {
+void loop()
+{
   // The core of your code will likely live here.
   Serial.println("loop");
   delay(1000);
