1cf66f858d8c9d282ec64ffd74568249a24f2462
Cliff Brake <cbrake@bec-systems.com>
2022-08-02 16:02:01 -0400

    add script for ctags generation

--- envsetup.sh
+++ envsetup.sh
@@ -6,6 +6,10 @@ siot_protobuf_gen() {
   (cd library/pb && python ../../nanopb/generator/nanopb_generator.py ./*.proto)
 }
 
+siot_ctags() {
+  arduino-ctags -R
+}
+
 # the following function needs to be run any time you update nanopb
 # to copy the library files to where arduino-cli can find them.
 siot_install_nanopb_library() {
