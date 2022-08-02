f3dee1ed8fadfea7aaf8cb7739307d8e4b8ef3b1
Cliff Brake <cbrake@bec-systems.com>
2022-08-02 15:57:36 -0400

    add script to install nanopb library files

--- envsetup.sh
+++ envsetup.sh
@@ -6,6 +6,13 @@ siot_protobuf_gen() {
   (cd library/pb && python ../../nanopb/generator/nanopb_generator.py ./*.proto)
 }
 
+# the following function needs to be run any time you update nanopb
+# to copy the library files to where arduino-cli can find them.
+siot_install_nanopb_library() {
+  cp nanopb/*.h libraries/nanopb/
+  cp nanopb/*.c libraries/nanopb/
+}
+
 siot_build_siot_mcu_serial() {
   (cd Arduino/SIOT_MCU_serial && arduino-cli compile \
     --libraries ../../libraries \
